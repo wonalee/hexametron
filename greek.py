18e160f548bfc9a8fa287d41c2540e0109e512cf
wonalee <wonalee1a@gmail.com>
2024-01-14 22:50:37 +0100

    모음 감별 함수 추가

--- greek.py
+++ greek.py
@@ -1,13 +1,4 @@
 """
-
-def isVowel(letter):
-	if (within 0x1F00 and 0x1FFF):
-		# greek extended, is vowel
-	else if (handpicked vowels):
-		# is vowel
-	else
-		is not vowel
-
 somewhere down the line, if there is vowels,
 diftongs becomes a concern
 
@@ -17,3 +8,28 @@ TODO:
 def isDiftong(something?):
 	#TODO
 """
+
+def isGreekExtended(letter):
+	# Checks if letter is within the Unicode Greek Extended block (polytonic orthography)
+	return ord(letter) in range(0x1F00, 0x1FFF + 1)
+
+def isGreekMonotonic(letter):
+	# Checks if letter corresponds to monotonic orthography
+	return ord(letter) in range(0x0386, 0x03CE + 1)
+
+# TODO: is there a way to remove diacritics?
+# 1. remove diacritics from all letters in greek extended block
+# 2. there will be overlaps, but we have all our vowels from that
+# 3. write tests to check if diacritic removal is working as we intend
+def isGreekMonotonicVowel(letter):
+	return letter in 'ΆΈΉΊΌΎΏΐΑΕΗΙΟΥΩΪΫάέήίΰαεηιουωϊϋόύώ'
+
+def isVowel(letter):
+	if isGreekExtended(letter):
+		# Unicode Greek Extended is guaranteed to be a vowel
+		return True
+	elif isGreekMonotonic(letter) and isGreekMonotonicVowel(letter):
+		return True
+
+	# is not vowel
+	return False
